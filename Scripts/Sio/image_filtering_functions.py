58d747fe0303ff6cd7b7676d8f582c10fef9aca7
Siokhan <siokhank@gmail.com>
2020-04-28 20:05:53 +0100

    pre processing lane detection functional, but needs testing

--- Scripts/Sio/image_filtering_functions.py
+++ Scripts/Sio/image_filtering_functions.py
@@ -7,10 +7,12 @@ Created on Thu Apr 23 17:30:34 2020
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 from matplotlib import patches
 
 #functions for image pre processing
 
+#### helper functions ####
 #function to create a figure of 2 images
 def double_figure(width, height, image1, image2, title1, title2, share_axis=True,
                   axis='off', tight=False):
@@ -86,13 +88,25 @@ def template_match_img(width, height, template, detected_img, result, peaks,
     
     return fig
 
-#lane edge detection
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+#### lane edge detection ####
 def detect_edges(frame):
-    # filter for blue lane lines
+    # filter for blue lane lines, invert the pixel values as it seems 
+    # to facilitate the process
     hsv = cv2.cvtColor(~frame, cv2.COLOR_BGR2HSV)
     #show_image("hsv", hsv)
     lower_black = np.array([0, 0, 0])
-    upper_black = np.array([180, 255, 230])
+    upper_black = np.array([180, 255, 250])
     mask = cv2.inRange(hsv, lower_black, upper_black)
     #show_image("black mask", mask)
 
@@ -105,14 +119,90 @@ def region_of_interest(edges):
     height, width = edges.shape
     mask = np.zeros_like(edges)
 
-    # only focus bottom half of the screen
+    # only focus bottom half of the screen and cut off the car bumper
     polygon = np.array([[
         (0, height * 1 / 2),
         (width, height * 1 / 2),
-        (width, height),
-        (0, height),
+        (width, 220),
+        (0, 220),
     ]], np.int32)
 
     cv2.fillPoly(mask, polygon, 255)
     cropped_edges = cv2.bitwise_and(edges, mask)
     return cropped_edges
+
+def detect_line_segments(cropped_edges):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+                                    np.array([]), minLineLength=8, maxLineGap=4)
+    #print(line_segments)
+
+    return line_segments
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    return lane_lines
+
+def detect_lane(frame):
+    
+    edges = detect_edges(frame)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines = average_slope_intercept(frame, line_segments)
+    
+    return lane_lines
+
+def display_lines(frame, lines, line_color=(239, 245, 66), line_width=4):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
+
